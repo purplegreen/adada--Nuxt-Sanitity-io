5cf95bcb89ce60c28f77262d035646f8d240a750
dehlix <deborahligorio@gmail.com>
2020-07-23 09:48:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# adada
+
+# Frontend | Nuxt.js
+
+# Headless CMS | Sanity.io
+
+## Usage
+
+To Start this Project
+
+Start Frontend
+To serve with hot reload at localhost:3000, run this command within the web folder:
+
+```
+npm run serve
+
+```
+
+Start CMS
+To start the development server for Sanity Studio, run this command within the studio folder:
+
+```
+sanity start
+
+```
---
+++ web/pages/index.vue
@@ -0,0 +1,119 @@
+<template>
+  <section>
+    <h1>DADA</h1>
+
+    <ul>
+      <li v-for="post in posts" :key="post._id" class="">
+        {{ post.title }}
+        <!-- <img v-if="post.mainImage" :src="post.mainImage" /> -->
+
+        <img
+          v-if="post.mainImage"
+          class="mainImage"
+          :src="imageUrlFor(post.mainImage).ignoreImageParams()"
+        />
+
+        <p class="block-content">
+          <block-content :blocks="post.body" />
+        </p>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import imageUrlBuilder from '@sanity/image-url'
+import BlockContent from 'sanity-blocks-vue-component'
+import sanity from '../sanity'
+
+const imageBuilder = imageUrlBuilder(sanity)
+const query = `*[_type == "post" ] | order(releaseDate desc)
+{
+  _id,
+  title,
+  mainImage,
+  imageUrl,
+  createdAt,
+  releaseDate,
+  body
+}[0...29]`
+
+export default {
+  name: 'Posts',
+  components: {
+    BlockContent,
+  },
+  data() {
+    return {
+      loading: true,
+      posts: [],
+    }
+  },
+  watch: {
+    $route: 'fetchData',
+  },
+  created() {
+    this.fetchData()
+  },
+  mounted() {},
+  methods: {
+    imageUrlFor(source) {
+      return imageBuilder.image(source)
+    },
+    fetchData() {
+      this.error = this.post = null
+      this.loading = true
+      sanity.fetch(query).then(
+        (posts) => {
+          this.posts = posts
+          // this.loading = false
+        },
+        (error) => {
+          this.error = error
+        }
+      )
+      // .then(() => {
+      //   this.startAnimation()
+      // })
+    },
+  },
+  head() {
+    return {
+      title: 'Listing',
+    }
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
